82be3ea85a91696a00d5a9dc8d99eebd3e76245f
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-05-16 19:16:23 +0300

    Removed Country code from settings

--- LawernaTestApplication/Models/ApplicationSettings.cs
+++ LawernaTestApplication/Models/ApplicationSettings.cs
@@ -2,7 +2,6 @@
 
 public class ApplicationSettings
 {
-    public string? Country { get; set; }
     public string? City { get; set; }
     public string? ApiKey { get; set; }
     public int UpdateInterval { get; set; }
