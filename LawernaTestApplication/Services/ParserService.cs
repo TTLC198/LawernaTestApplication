c6b2191d2efbc60803e258d2330793c8b0c9fb20
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-05-16 18:42:35 +0300

    Added services for interacting with api

---
+++ LawernaTestApplication/Services/ParserService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text.Json;
+using System.Threading;
+using AnySoftDesktop.Utils;
+using HSMonitor.Services;
+using LawernaTestApplication.Models;
+
+namespace LawernaTestApplication.Services;
+
+public class ParserService
+{
+    private readonly SettingsService _settingsService;
+    public WeatherData WeatherData { get; set; }
+
+    public ParserService(SettingsService settingsService)
+    {
+        _settingsService = settingsService;
+        _settingsService.SettingsSaved += SettingsServiceOnSettingsSaved;
+    }
+    
+    private void SettingsServiceOnSettingsSaved(object? sender, EventArgs e)
+    {
+        if (sender is not SettingsService service) return;
+        _settingsService.Settings = service.Settings;
+    }
+
+    public event EventHandler? WeatherInformationUpdated;
+    
+    public async void WeatherInformationUpdate()
+    {
+        try
+        {
+            var getProductsRequest = await WebApiService.GetCall($"?q={_settingsService.Settings.City}&units=metric&appid={_settingsService.Settings.ApiKey}");
+            if (getProductsRequest.IsSuccessStatusCode)
+            {
+                var timeoutAfter = TimeSpan.FromMilliseconds(3000);
+                using var cancellationTokenSource = new CancellationTokenSource(timeoutAfter);
+                var responseStream =
+                    await getProductsRequest.Content.ReadAsStreamAsync(cancellationTokenSource.Token);
+                var weatherData = await JsonSerializer.DeserializeAsync<WeatherData>(
+                    responseStream,
+                    CustomJsonSerializerOptions.Options, cancellationToken: cancellationTokenSource.Token);
+                WeatherData = weatherData ?? throw new InvalidOperationException("Weather data is null");
+            }
+            else
+            {
+                var msg = await getProductsRequest.Content.ReadAsStringAsync();
+                throw new InvalidOperationException($"{getProductsRequest.ReasonPhrase}\n{msg}");
+            }
+        }
+        catch
+        {
+            // ignored
+        }
+        finally
+        {
+            OnWeatherInformationUpdated();
+        }
+    }
+    
+    private void OnWeatherInformationUpdated()
+    {
+        WeatherInformationUpdated?.Invoke(this, EventArgs.Empty);
+    }
+}
---
+++ LawernaTestApplication/Services/WebApiService.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace LawernaTestApplication.Services;
+
+public class WebApiService
+{
+    public static async Task<HttpResponseMessage> GetCall(string url, string authorizationToken = "")  
+    {  
+        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;  
+        var apiUrl = App.ApiUrl + url;
+        try
+        {
+            using var client = new HttpClient();
+            if (authorizationToken != string.Empty)
+                client.DefaultRequestHeaders.Add("Authorization", $"Bearer {authorizationToken}");
+            client.BaseAddress = new Uri(apiUrl);  
+            client.Timeout = TimeSpan.FromSeconds(900);  
+            client.DefaultRequestHeaders.Accept.Clear();  
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));  
+            var response = await client.GetAsync(apiUrl);   
+            return response;
+        }
+        catch
+        {
+            throw;
+        }
+    } 
+    
+    public static async Task<HttpResponseMessage> PostCall<T>(
+        string url,
+        T model,
+        string? authorizationToken = null,
+        string? contentType = null) where T : class
+    {
+        var apiUrl = App.ApiUrl + url;
+        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+        try
+        {
+            using var client = new HttpClient();
+            if (authorizationToken is not null)
+                client.DefaultRequestHeaders.Add("Authorization", $"Bearer {authorizationToken}");
+            client.BaseAddress = new Uri(apiUrl);  
+            client.Timeout = TimeSpan.FromSeconds(900);  
+            client.DefaultRequestHeaders.Accept.Clear();  
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(contentType ??= "application/json"));  
+            var response = await client.PostAsJsonAsync(apiUrl, model);  
+            return response;
+        }
+        catch
+        {
+            throw;
+        }
+    }  
+    
+    public static async Task<HttpResponseMessage> PostCall(
+        string url,
+        MultipartFormDataContent content,
+        string? authorizationToken = null,
+        string? contentType = null)
+    {
+        var apiUrl = App.ApiUrl + url;
+        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+        try
+        {
+            using var client = new HttpClient();
+            if (authorizationToken is not null)
+                client.DefaultRequestHeaders.Add("Authorization", $"Bearer {authorizationToken}");
+            client.BaseAddress = new Uri(apiUrl);  
+            client.Timeout = TimeSpan.FromSeconds(900);  
+            client.DefaultRequestHeaders.Accept.Clear();  
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(contentType ??= "application/json"));  
+            var response = await client.PostAsync(apiUrl, content);  
+            return response;
+        }
+        catch
+        {
+            throw;
+        }
+    }  
+    
+    public static async Task<HttpResponseMessage> PutCall<T>(
+        string url,
+        T model,
+        string? authorizationToken = null) where T : class  
+    {  
+        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;  
+        var apiUrl = App.ApiUrl + url;
+        try
+        {
+            using var client = new HttpClient();
+            if (authorizationToken is not null)
+                client.DefaultRequestHeaders.Add("Authorization", $"Bearer {authorizationToken}");
+            client.BaseAddress = new Uri(apiUrl);  
+            client.Timeout = TimeSpan.FromSeconds(900);  
+            client.DefaultRequestHeaders.Accept.Clear();  
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));  
+            var response = await client.PutAsJsonAsync(apiUrl, model); 
+            return response;
+        }
+        catch
+        {
+            throw;
+        }
+    }  
+    
+    public static async Task<HttpResponseMessage> DeleteCall(
+        string url,
+        string? authorizationToken = null)   
+    {  
+        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;  
+        var apiUrl = App.ApiUrl + url;
+        try
+        {
+            using var client = new HttpClient();
+            if (authorizationToken is not null)
+                client.DefaultRequestHeaders.Add("Authorization", $"Bearer {authorizationToken}");
+            client.BaseAddress = new Uri(apiUrl);  
+            client.Timeout = TimeSpan.FromSeconds(900);  
+            client.DefaultRequestHeaders.Accept.Clear();  
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));  
+            var response = await client.DeleteAsync(apiUrl);
+            return response;
+        }
+        catch
+        {
+            throw;
+        }
+    } 
+}
