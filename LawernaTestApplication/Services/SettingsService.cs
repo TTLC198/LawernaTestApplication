b310bda7e99e17454606efd41a1667b84f14cf3f
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-05-16 18:42:45 +0300

    Added service for interacting with settings

---
+++ LawernaTestApplication/Services/SettingsService.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Windows;
+using LawernaTestApplication;
+using LawernaTestApplication.Models;
+using LawernaTestApplication.Utils;
+using LawernaTestApplication.ViewModels;
+using LawernaTestApplication.ViewModels.Framework;
+
+namespace HSMonitor.Services;
+
+public class SettingsService
+{
+    private readonly IViewModelFactory _viewModelFactory;
+    private readonly DialogManager _dialogManager;
+    
+    public event EventHandler? SettingsReset;
+    
+    public event EventHandler? SettingsLoaded;
+    
+    public event EventHandler? SettingsSaved;
+    
+    public ApplicationSettings Settings { get; set; } = null!;
+
+    private readonly string _configurationPath = Path.Combine(App.ExecutableDirPath, "appsettings1.json");
+
+    public SettingsService(IViewModelFactory viewModelFactory, DialogManager dialogManager)
+    {
+        _viewModelFactory = viewModelFactory;
+        _dialogManager = dialogManager;
+        Load();
+    }
+    
+    public void Reset()
+    {
+        Settings = new ApplicationSettings()
+        {
+            Country = null,
+            City = null,
+            ApiKey = null,
+            UpdateInterval = 1000
+        };
+        Save();
+        SettingsReset?.Invoke(this, EventArgs.Empty);
+    }
+
+    public void Load()
+    {
+        try
+        {
+            var settingsJson = File.ReadAllText(_configurationPath);
+            Settings = JsonSerializer.Deserialize<ApplicationSettings>(settingsJson) ?? throw new InvalidOperationException();
+            SettingsLoaded?.Invoke(this, EventArgs.Empty);
+        }
+        catch (Exception exception)
+        {
+            Task.Run(async () =>
+            {
+                var messageBoxDialog = _viewModelFactory.CreateMessageBoxViewModel(
+                    title: "Some error has occurred",
+                    message: $@"
+An error has occurred, the error text is shown below
+{exception.Message}".Trim(),
+                    okButtonText: "OK",
+                    cancelButtonText: null
+                );
+
+                if (await _dialogManager.ShowDialogAsync(messageBoxDialog) == true)
+                    Application.Current.Shutdown();
+            });
+        }
+    }
+
+    public void Save()
+    {
+        Settings.JsonToFile(_configurationPath);
+        
+        SettingsSaved?.Invoke(this, EventArgs.Empty);
+    }
+}
