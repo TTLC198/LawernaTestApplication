3119d6c2fe60608f29f20daea71698482fc27342
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-05-16 18:41:34 +0300

    Added some models

---
+++ LawernaTestApplication/Models/ApplicationSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace LawernaTestApplication.Models;
+
+public class ApplicationSettings
+{
+    public string? Country { get; set; }
+    public string? City { get; set; }
+    public string? ApiKey { get; set; }
+    public int UpdateInterval { get; set; }
+}
